22b142857b304643771c1ddd67f961dd22303af5
NobuMayuri <faygodrinkergamzee@gmail.com>
2024-03-13 20:47:42 -0700

    Finished the bits portion of this project'

--- bits.c
+++ bits.c
@@ -8,32 +8,38 @@
 
 // Get the nth bit
 uint16_t getbit(uint16_t number, int n) {
-    // TODO: Your code here
-    return 0;
+    return (number & (1 << n)) >> n;
 }
 
 // Get bits that are the given number of bits wide
 uint16_t getbits(uint16_t number, int n, int wide) {
-    // TODO: Your code here
-    return 0;
+    number >>= n;
+    number &= ~(1<<wide);
+    number&(1<<(wide-1));
+    return number;
 }
 
 // Set the nth bit to the given bit value and return the result
 uint16_t setbit(uint16_t number, int n) {
     // TODO: Your code here
-    return 0;
+    return (number | 1 << n);
 }
 
 // Clear the nth bit
 uint16_t clearbit(uint16_t number, int n) {
-    // TODO: Your code here
-    return 0;
+    return (number & ~(1 << n));
 }
 
 // Sign extend a number of the given bits to 16 bits
 uint16_t sign_extend(uint16_t x, int bit_count) {
-    // TODO: Your code here
-    return 0;
+    uint16_t leading = (1 << (bit_count-1)) & x;
+    uint16_t replace = ((~0) >> (bit_count-1)) << (bit_count-1);
+    if (leading != 0){
+        x |= replace;
+    } else {
+        x &= ~replace;
+    }
+    return x;
 }
 
 bool is_positive(uint16_t number) {
